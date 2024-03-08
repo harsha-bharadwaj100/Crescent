175ed7c53e0fd8185d3e49d09fdbb58d179551fe
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-03-08 16:32:58 +0530

    app

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
       <StatusBar style="auto" />
     </View>
+    // <FlatList></FlatList>
   );
 }
 
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
